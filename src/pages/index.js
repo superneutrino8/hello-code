3aeabae8b561c17fba37fd044d90737d726312b9
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-07-10 12:24:56 +0530

    base template ready

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Learn To Design and Code React Apps</h1>
+    <p>
+      Complete Course about the best tools and design systems. Prototype and
+      built apps with React and Swift.
+    </p>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
