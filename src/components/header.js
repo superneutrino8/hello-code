78f2e2382734be0e516eaf8bcac6b22f559b5f55
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-07-16 14:23:58 +0530

    added bg to header on scroll

--- src/components/header.js
+++ src/components/header.js
@@ -2,30 +2,50 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-import './Header.css';
+import "./Header.css"
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/">
-        <img src={require('../images/logo-designcode.svg')} alt="HelloCode Logo" />
-      </Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/downloads">Downloads</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/buy">
-        <button>Buy</button>
-      </Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasScrolled: false
+    }
+  }
 
-Header.propTypes = {
-  siteTitle: PropTypes.string
-}
+  handleScroll = e => {
+    const scrollTop = window.pageYOffset
 
-Header.defaultProps = {
-  siteTitle: ``
+    this.setState({
+      hasScrolled: scrollTop > 50 ? true : false
+    })
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+  }
+
+  render() {
+    return (
+      <div
+        className={this.state.hasScrolled ? "Header HeaderScrolled" : "Header"}
+      >
+        <div className="HeaderGroup">
+          <Link to="/">
+            <img
+              src={require("../images/logo-designcode.svg")}
+              alt="HelloCode Logo"
+            />
+          </Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy">
+            <button>Buy</button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Header
